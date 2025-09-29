1d80c1e61a91e46df2d8249f59e1e69c627b9004
armaan240 <kiransonischool@gmail.com>
2025-09-29 05:54:14 +0530

    Finalized the whole website ,decided to make it look like a retro game ledger add css and worked on responsivness of the website

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,30 +1,31 @@
-import { Link ,useLocation} from "react-router-dom";
-import "../styles/Navbar.css";
 
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import "../styles/Navbar.css";
 function Navbar() {
   const location = useLocation();
+const navigate=useNavigate();
+    function handleReset(){
+        localStorage.clear();
+        navigate("/");
+    }
   return (
     <nav className="navbar">
-      <h1 className="logo">Expense Tracker</h1>
+      <h1 className="logo">Expenzo</h1>
       <ul className="nav-links">
-        <li className={location.pathname==="/"?"active":""}>
-          <Link to="/">📊 Dashboard</Link>
+        <li className={location.pathname === "/" ? "active" : ""}>
+          <Link to={"/"}>📊 Dashboard</Link>
         </li>
-        <li className={location.pathname==="/transaction"?"active":""}>
-          <Link to="/transaction">💳 Transaction</Link>
+        <li className={location.pathname === "/transaction" ? "active" : ""}>
+          <Link to={"/transaction"}>📄 Transaction</Link>
         </li>
-        <li className={location.pathname==="/reports"?"active":""}>
-          <Link to="/reports">📈 Reports</Link>
+        <li className={location.pathname === "/reports" ? "active" : ""}>
+          <Link to={"/reports"}>⏳ Reports</Link>
         </li>
-        <li className={location.pathname==="/getquote"?"active":""}>
-          <Link to="/getquote">💬 Get Quote</Link>
-        </li>
-        <li className={location.pathname==="/reset"?"active":""}>
-          <Link to="/reset">🔄 Reset</Link>
+        <li>
+        <div className="reset-btn" onClick={handleReset}>🔄 Reset</div>
         </li>
       </ul>
     </nav>
   );
 }
-
 export default Navbar;
